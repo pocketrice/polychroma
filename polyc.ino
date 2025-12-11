303beac69fed6ffc3b23c2a1ee271e26d5386b9b
pocketrice <79682953+pocketrice@users.noreply.github.com>
2025-12-10 21:00:56 -0500

    polyc.ino compiler pass
    Having issues with audio playback so will be testing w/o for now.

--- polyc.ino
+++ polyc.ino
@@ -1,62 +1,63 @@
 #include <IRremoteESP8266.h>
 #include <IRrecv.h>
-#include "AudioFileSourceSD_MMC.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
+#include "AudioFileSourceSD.h"
 #include "AudioOutputI2S.h"
 #include "AudioGeneratorMP3.h"
 #include "SD_MMC.h"
 #include "FS.h"
 
-#define PIN_JOY_X 36;
-#define PIN_JOY_Y 39;
-#define PIN_JOY_SW 23;
+const int PIN_JOY_X = 36;
+const int PIN_JOY_Y = 39;
+const int PIN_JOY_SW = 23;
 
 //#define PIN_IR_OUT  35;
-#define PIN_IR_IN  32;
+const int PIN_IR_IN = 32;
 
-#define PIN_LED_R  21;
-#define PIN_LED_G  19;
-#define PIN_LED_B  18;
+const int PIN_LED_R = 21;
+const int PIN_LED_G = 19;
+const int PIN_LED_B = 18;
 
-#define PIN_AMP_MONO  22;
+const int PIN_AMP_MONO = 22;
 
-#define PIN_TFT_LITE  33;
-#define PIN_TFT_SCLK  25;
-#define PIN_TFT_MOSI  26;
-#define PIN_TFT_CS  27;
-#define PIN_TFT_DC  14;
-#define PIN_TFT_RESET  13;
+const int PIN_TFT_LITE = 33;
+const int PIN_TFT_SCLK = 25;
+const int PIN_TFT_MOSI = 26;
+const int PIN_TFT_CS = 27;
+const int PIN_TFT_DC = 14;
+const int PIN_TFT_RESET = 13;
 
-IRrecv irrecv(IR_RECEIVE_PIN);
+IRrecv irrecv(PIN_IR_IN);
 decode_results irres;
 
 AudioGeneratorMP3 *mp3;
-AudioFileSourceSD_MMC *file_l0;
-AudioFileSourceSD_MMC *file_l1;
-AudioFileSourceSD_MMC *file_l2;
-AudioFileSourceSD_MMC *file_l3;
+AudioFileSourceSD *file_l0;
+AudioFileSourceSD *file_l1;
+AudioFileSourceSD *file_l2;
+AudioFileSourceSD *file_l3;
 AudioOutputI2S *out;
 
 // "game" vars
-AudioFileSourceSD_MMC *level_audio;
-uint8_t *level_map;
-uint8_t *level_mapdata;
-uint16_t *level_pal;
+int level_id;
+AudioFileSourceSD *level_audio = nullptr;
+uint8_t *level_map = nullptr; // technically `const`
+uint8_t *level_mapdata = nullptr;
+const uint16_t *level_pal = nullptr;
 uint8_t player_x;
 uint8_t player_y;
 uint8_t door0_x;
 uint8_t door0_y;
 uint8_t door1_x;
 uint8_t door1_y;
-std::string tft_lid; // level id 
 
 bool is_special;
 uint8_t special_tiles_left;
 bool is_game;
 int game_end_state; // 0 = not end, 1 = OK, 2 = NG
-enum JoyDir joydir;
-uint8_t mapdata4[80];
 
-Adafruit_ST7735 tft = Adafruit_ST7735(PIN_TFT_CS, PIN_TFT_DC, PIN_TFT_RST);
+Adafruit_ST7735 tft = Adafruit_ST7735(PIN_TFT_CS, PIN_TFT_DC, PIN_TFT_RESET);
 
 // 0 = empty, 1 = wall, 2 = door, 3 = lava, 4 = (special) paint
 // first six are (1) start tile x/y and (2) exit tile A/B x/y. note first tile is always reserved for level ID and overwritten.
@@ -69,7 +70,7 @@ const uint8_t TILE_PIXELS_HOR = GBC_PIXELS_HOR / LEVEL_TILES_HOR; // shoould be 
 const uint8_t TILE_PIXELS_VERT = GBC_PIXELS_VERT / LEVEL_TILES_VERT; // should be 16
 const uint16_t palette_player = tft.color565(198, 191, 214); // color of the player tile
 
-const uint8_t levelmap_0[84] = { // please note!! mapdata is y-inverted for easy ptr arithmetic.
+uint8_t levelmap_0[86] = { // please note!! mapdata is y-inverted for easy ptr arithmetic.
   0, 1,
   8, 6, 8, 5,
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -80,7 +81,7 @@ const uint8_t levelmap_0[84] = { // please note!! mapdata is y-inverted for easy
   0, 0, 0, 0, 0, 3, 0, 0, 2, 0,
   0, 0, 0, 0, 0, 3, 0, 0, 2, 0,
   0, 0, 0, 0, 0, 3, 0, 0, 0, 0,
-}
+};
 
 const uint16_t palette_0[5] = {
   tft.color565(0, 0, 0),
@@ -88,9 +89,9 @@ const uint16_t palette_0[5] = {
   tft.color565(91, 110, 225),
   tft.color565(172, 50, 50),
   tft.color565(0, 0, 0) // no special
-}
+};
 
-const uint8_t levelmap_1[84] = {
+uint8_t levelmap_1[86] = {
   1, 7,
   8, 4, 8, 5,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -101,20 +102,20 @@ const uint8_t levelmap_1[84] = {
   0, 3, 0, 0, 0, 0, 1, 1, 2, 0,
   0, 0, 0, 3, 0, 0, 0, 1, 1, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-}
+};
 
-const uint16_t palette_1[5] = {
+uint16_t palette_1[5] = {
   tft.color565(34, 32, 52),
   tft.color565(238, 195, 154),
   tft.color565(34, 32, 52), // door is "hidden"
   tft.color565(154, 178, 238),
   tft.color565(0, 0, 0) // no special
-}
+};
 
-const uint8_t levelmap_2[84] = {
+uint8_t levelmap_2[86] = {
   9, 7,
   5, 3, 5, 4,
-  3, 3, 0, 3, 0, 0, 0, 0, 0, 0
+  3, 3, 0, 3, 0, 0, 0, 0, 0, 0,
   0, 3, 3, 0, 3, 3, 3, 0, 0, 0,
   0, 3, 3, 3, 3, 3, 3, 3, 0, 0,
   0, 3, 3, 0, 0, 2, 3, 3, 3, 3,
@@ -122,7 +123,7 @@ const uint8_t levelmap_2[84] = {
   3, 3, 3, 0, 3, 3, 3, 3, 3, 0,
   0, 0, 3, 0, 0, 3, 3, 3, 0, 0,
   3, 0, 0, 3, 0, 3, 0, 0, 0, 0,
-}
+};
 
 const uint16_t palette_2[5] = {
   tft.color565(16, 65, 67),
@@ -130,13 +131,13 @@ const uint16_t palette_2[5] = {
   tft.color565(91, 110, 225),
   tft.color565(126, 193, 197),
   tft.color565(0, 0, 0) // no special
-}
+};
 
-const uint8_t levelmap_3[6] = {
+uint8_t levelmap_3[6] = {
   1, 7,
   100, 100, 100, 100, // no exit door
   // [[ mutable level data @ levelmap4 ]]
-}
+};
 
 const uint16_t palette_3[5] = {
   tft.color565(0, 0, 0), // yes blank, replaced after painted
@@ -144,7 +145,7 @@ const uint16_t palette_3[5] = {
   tft.color565(255, 255, 255), // no door
   tft.color565(255, 255, 255), // no lava
   tft.color565(255, 28, 127), // yes special!
-}
+};
 
 const int pwmFreq = 5000;
 const int pwmRes = 8;
@@ -176,10 +177,10 @@ void setup() {
   irrecv.enableIRIn();
 
   SD_MMC.begin();
-  file_l0 = new AudioFileSourceSD_MMC("/l0.mp3");
-  file_l1 = new AudioFileSourceSD_MMC("/l1.mp3");
-  file_l2 = new AudioFileSourceSD_MMC("/l2.mp3");
-  file_l3 = new AudioFileSourceSD_MMC("/l3.mp3");
+  file_l0 = new AudioFileSourceSD("/l0.mp3");
+  file_l1 = new AudioFileSourceSD("/l1.mp3");
+  file_l2 = new AudioFileSourceSD("/l2.mp3");
+  file_l3 = new AudioFileSourceSD("/l3.mp3");
   out = new AudioOutputI2S(0, 1);
   out->SetOutputModeMono(true);
   mp3 = new AudioGeneratorMP3();
@@ -188,8 +189,8 @@ void setup() {
 }
 
 void loop() {
-  if (!is_game && irrecv.decode(&results)) {
-      int level_id = initGame(decodeIRRes(irres.value), joy_x, joy_y, joy_btn);
+  if (!is_game && irrecv.decode(&irres)) {
+      level_id = decodeIRRes(irres.value);
       
       switch (level_id) {
         case 0:
@@ -198,7 +199,6 @@ void loop() {
           level_pal = palette_0;
           is_special = false;
           is_game = true;
-          tft_lid = "0"
           break;
 
         case 1:
@@ -207,7 +207,6 @@ void loop() {
           level_pal = palette_1;
           is_special = false;
           is_game = true;
-          tft_lid = "1"
           break;
 
         case 2:
@@ -216,7 +215,6 @@ void loop() {
           level_pal = palette_2;
           is_special = false;
           is_game = true;
-          tft_lid = "2"
           break;
     
         case 3: // should be only 3
@@ -225,7 +223,6 @@ void loop() {
           level_pal = palette_3;
           is_special = true;
           is_game = true;
-          tft_lid = "3";
           break;
 
         default:
@@ -238,29 +235,25 @@ void loop() {
       door0_y = *(level_map + 3);
       door1_x = *(level_map + 4);
       door1_y = *(level_map + 5);
-      level_mapdata = (is_special) ? levelmap4 : level_map + 6;
+      level_mapdata = (is_special) ? new uint8_t[80] {
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+        } : level_map + 6;
 
       // set sync LED
       setColor(activeRGB);
 
       // reset game vars
       game_end_state = 0;
+      special_tiles_left = 80;
 
-      if (is_special) {
-        special_tiles_left = 80;
-
-        mapdata4 = {
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-          4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-        }
-      }
-
+      // start the tunes!!
       mp3->begin(level_audio);
   }
 
@@ -274,31 +267,31 @@ void loop() {
     int player_ox = player_x; // <-- store previous positions if need to reset after collision check.
     int player_oy = player_y;
 
-    if (yValue <= JOY_LOW_EDGE && xValue >= JOY_MID_LBOUND && xValue <= JOY_MID_RBOUND) { // (1800, 0) :~ (1700-1900, 0-200). UP!
+    if (joy_y <= JOY_LOW_EDGE && joy_x >= JOY_MID_LBOUND && joy_x <= JOY_MID_RBOUND) { // (1800, 0) :~ (1700-1900, 0-200). UP!
       player_y = (player_y == LEVEL_TILES_VERT - 1) ? 0 : player_y + 1; // <-- wrap if needed
 
-    } else if (xValue >= JOY_MID_LBOUND && xValue <= JOY_MID_RBOUND && yValue >= JOY_HI_EDGE) { // (1800, 4095) :~ (1700-1900, 3900-4095). DOWN!
+    } else if (joy_x >= JOY_MID_LBOUND && joy_x <= JOY_MID_RBOUND && joy_y >= JOY_HI_EDGE) { // (1800, 4095) :~ (1700-1900, 3900-4095). DOWN!
       player_y = (player_y == 0) ? LEVEL_TILES_VERT - 1 : player_y - 1;
 
-    } else if (xValue >= JOY_HI_EDGE && yValue >= JOY_MID_LBOUND && yValue <= JOY_MID_RBOUND) { // (4095, 1800) :~ (3900-4095, 1700-1900). RIGHT!
+    } else if (joy_x >= JOY_HI_EDGE && joy_y >= JOY_MID_LBOUND && joy_y <= JOY_MID_RBOUND) { // (4095, 1800) :~ (3900-4095, 1700-1900). RIGHT!
       player_x = (player_x == LEVEL_TILES_HOR - 1) ? 0 : player_x + 1;
 
-    } else if (xValue <= JOY_LOW_EDGE && yValue >= JOY_MID_LBOUND && yValue <= JOY_MID_RBOUND) { // (0, 1800) :~ (0-200, 1700-1900). LEFT!
+    } else if (joy_x <= JOY_LOW_EDGE && joy_y >= JOY_MID_LBOUND && joy_y <= JOY_MID_RBOUND) { // (0, 1800) :~ (0-200, 1700-1900). LEFT!
       player_x = (player_x == 0) ? LEVEL_TILES_HOR - 1 : player_x - 1;
 
-    } else if (xValue <= JOY_LOW_EDGE && yValue >= JOY_HI_EDGE) { // (0, 4095) :~ (0-200, 3900-4095). DOWN_LEFT!
+    } else if (joy_x <= JOY_LOW_EDGE && joy_y >= JOY_HI_EDGE) { // (0, 4095) :~ (0-200, 3900-4095). DOWN_LEFT!
       player_x = (player_x == 0) ? LEVEL_TILES_HOR - 1 : player_x - 1;
       player_y = (player_y == 0) ? LEVEL_TILES_VERT - 1 : player_y - 1;
 
-    } else if (xValue >= JOY_HI_EDGE && yValue >= JOY_HI_EDGE) { // (4095, 4095) :~ (3900-4095, 3900-4095). DOWN_RIGHT!
+    } else if (joy_x >= JOY_HI_EDGE && joy_y >= JOY_HI_EDGE) { // (4095, 4095) :~ (3900-4095, 3900-4095). DOWN_RIGHT!
       player_x = (player_x == LEVEL_TILES_VERT - 1) ? 0 : player_x + 1;
       player_y = (player_y == 0) ? LEVEL_TILES_VERT - 1 : player_y - 1;
 
-    } else if (xValue <= JOY_LOW_EDGE && yValue <= JOY_LOW_EDGE) { // (0, 0) :~ (0-200, 0-200). UP_LEFT!
+    } else if (joy_x <= JOY_LOW_EDGE && joy_y <= JOY_LOW_EDGE) { // (0, 0) :~ (0-200, 0-200). UP_LEFT!
       player_x = (player_x == 0) ? LEVEL_TILES_HOR - 1 : player_x - 1;
       player_y = (player_y == LEVEL_TILES_VERT - 1) ? 0 : player_y + 1;
 
-    } else if (xValue >= JOY_HI_EDGE && yValue <= JOY_LOW_EDGE) { // (4095, 0) :~ (3900-4095, 0-200). UP_RIGHT!
+    } else if (joy_x >= JOY_HI_EDGE && joy_y <= JOY_LOW_EDGE) { // (4095, 0) :~ (3900-4095, 0-200). UP_RIGHT!
       player_x = (player_x == LEVEL_TILES_HOR - 1) ? 0 : player_x + 1;
       player_y = (player_y == LEVEL_TILES_VERT - 1) ? 0 : player_y + 1;
 
@@ -333,11 +326,11 @@ void loop() {
     // render frame
     for (size_t x = 0; x < LEVEL_TILES_HOR; x++) {
       for (size_t y = 0; y < LEVEL_TILES_VERT; y++) {
-        tft.drawRect(x * TILE_PIXELS_HOR, y * TILE_PIXELS_VERT, (x+1) * TILE_PIXELS_HOR, (y+1) * TILE_PIXELS_VERT, *(level_pal[*(level_mapdata + y * LEVEL_TILES_VERT + x)]));
+        tft.fillRect(x * TILE_PIXELS_HOR, y * TILE_PIXELS_VERT, (x+1) * TILE_PIXELS_HOR, (y+1) * TILE_PIXELS_VERT, level_pal[*(level_mapdata + y * LEVEL_TILES_VERT + x)]);
       }
     }
-    tft.println(tft_lid) // overdraw level num
-    tft.drawRect(player_x * TILE_PIXELS_HOR, player_y * TILE_PIXELS_VERT, (player_x+1) * TILE_PIXELS_HOR, (player_y+1) * TILE_PIXELS_VERT, palette_player); // overdraw player
+    tft.drawChar(0, 0, '0' + level_id, ST77XX_WHITE, ST77XX_BLACK, 16) // overdraw level num
+    tft.fillRect(player_x * TILE_PIXELS_HOR, player_y * TILE_PIXELS_VERT, (player_x+1) * TILE_PIXELS_HOR, (player_y+1) * TILE_PIXELS_VERT, palette_player); // overdraw player
 
     // sync with standard 60 fps
     delay(16);
@@ -348,6 +341,12 @@ void loop() {
       mp3->stop();
       setColor(standbyRGB);
       is_game = false;
+
+      // clean up `new` malloc
+      if (is_special) {
+        delete[] level_mapdata;
+        level_mapdata = nullptr;
+      }
     }
   }
 }
@@ -367,7 +366,7 @@ uint8_t decodeIRRes(long value) {
   }
 }
 
-void setColor(int rgb[3]) {
+void setColor(const int *rgb) {
   ledcWrite(PIN_LED_R, rgb[0]);
   ledcWrite(PIN_LED_G, rgb[1]);
   ledcWrite(PIN_LED_B, rgb[2]);
